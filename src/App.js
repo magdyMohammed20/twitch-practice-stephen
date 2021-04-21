89a6e36a5a56c344d9898d0a9ffe110415a72a1f
magdyMohammed20 <magdymohammed37@yahoo.com>
2021-04-21 15:20:37 +0200

    Install React Router And Setup Basic Usage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
+import {BrowserRouter , Route} from 'react-router-dom'
+
+const pageone = () => {
+  return <div>Page One</div>
+}
+
+const pagetwo = () => {
+  return <div>Page Two</div>
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Route path="/" exact component={pageone} />
+        <Route path="/pagetwo" component={pagetwo} />
+      </BrowserRouter>      
     </div>
   );
 }
